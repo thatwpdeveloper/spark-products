81f8df1cf498969cf01bde30d0382477ba40a296
Dmitri <dmitri@Dmitris-MacBook-Pro.local>
2019-04-18 21:52:22 +0100

    Added plugin header

---
+++ spark-products.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @link              https://github.com/thatwpdeveloper
+ * @since             1.0.0
+ * @package           Spark_Products
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Spark Products
+ * Plugin URI:        https://github.com/thatwpdeveloper/spark-products
+ * Description:       A basic WordPress plugin that will allow you to display products from certain categories.
+ * Version:           1.0.0
+ * Author:            Dmitri Bostan
+ * Author URI:        https://github.com/thatwpdeveloper
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       spark-products
+ * Domain Path:       /languages
+ */
