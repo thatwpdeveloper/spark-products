41cd0a96850aadb144352da94eb64573fd43a20c
Dmitri Bostan <thatwpdeveloper@gmail.com>
2019-04-20 19:48:12 +0100

    Added comments to rating helper class

--- includes/helpers/class-spark-products-rating.php
+++ includes/helpers/class-spark-products-rating.php
@@ -1,8 +1,25 @@
 <?php
 
-// TODO: Documentation!
+/**
+ * Manages the handling of rating output.
+ *
+ * @since      1.0.0
+ * @package    Spark_Products
+ * @subpackage Spark_Products/includes
+ */
+
 class Spark_Products_Rating {
 
+	/**
+	 * Displays the rating in form of stars.
+	 *
+	 * If rating is 3, it will display 3 full stars and 2 empty stars.
+	 *
+	 * @since 1.0.0
+	 * @param $post_id
+	 * @param $meta_key
+	 * @return string
+	 */
 	public static function star_rating( $post_id, $meta_key ) {
 
 		$rating = (int) get_post_meta( $post_id, $meta_key, true );
