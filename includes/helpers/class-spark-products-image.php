27f917f75a5b43ee06b3af7e56df9742db410804
Dmitri Bostan <thatwpdeveloper@gmail.com>
2019-04-20 19:41:57 +0100

    Added comments to image helper class

--- includes/helpers/class-spark-products-image.php
+++ includes/helpers/class-spark-products-image.php
@@ -1,9 +1,25 @@
 <?php
 
-// TODO: Documentation!
+/**
+ * Manages the handling of image output.
+ *
+ * @since      1.0.0
+ * @package    Spark_Products
+ * @subpackage Spark_Products/includes
+ */
 
 class Spark_Products_Image {
 
+	/**
+	 * Returns the image that comes from an non-serialised post_meta field.
+	 *
+	 * @since 1.0.0
+	 * @param $post_id
+	 * @param $meta_key
+	 * @param $alt_text
+	 *
+	 * @return string
+	 */
 	public static function from_post_meta( $post_id, $meta_key, $alt_text ) {
 		$src = get_post_meta( $post_id, $meta_key, true );
 
